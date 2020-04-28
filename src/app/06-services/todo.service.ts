958efc9867d7abd1ef9f337b149f027a0575fd32
szinta <szinta@gmail.com>
2020-04-28 15:15:06 +0100

    use cases

---
+++ src/app/06-services/todo.service.ts
@@ -0,0 +1,20 @@
+
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+
+export class TodoService { 
+  constructor(private http: HttpClient) { 
+  }
+
+  add(todo) {
+    return this.http.post('...', todo);
+  }
+
+  getTodos() { 
+    return this.http.get('...');
+  }
+
+  delete(id) {
+    return this.http.delete('...');
+  }
+}
